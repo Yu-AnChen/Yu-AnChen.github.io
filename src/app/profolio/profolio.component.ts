49df9e7cb192f15807e08bd96b84bd2aa857e57d
Yu-An Chen <atwood12@gmail.com>
2017-04-13 02:07:41 -0700

    profolio component

---
+++ src/app/profolio/profolio.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { dataArr } from './data';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-profolio',
+  templateUrl: './profolio.component.html',
+  styleUrls: ['./profolio.component.css']
+})
+export class ProfolioComponent {
+  // @ViewChild('acc') acc;
+  data: any = dataArr;
+  showDetail = {};
+  category: String;
+  nav: Array<String> = [ 'research', 'code'/*, 'baking'*/];
+  route: any;
+
+  constructor(route: Router) {
+    this.route = route;
+    console.log(route.routerState);
+    console.log(route);
+    console.log(route.url.split('/')[1]);
+  }
+
+  ngOnInit() {
+    this.category = this.route.url.split('/')[1];
+  }
+  changeCategory(str) {
+    if (!str) { return; }
+    return this.category = str;
+  }
+
+  getImgUrl(str) {
+    if (!str) { return; }
+    return `assets/img/${str}`;
+  }
+}
