cf9a45043040faa60f087b5b475ab16c58b7ee7f
Yu-An Chen <atwood12@gmail.com>
2017-04-16 01:36:12 -0700

    add links for publications

--- src/app/profolio/data.ts
+++ src/app/profolio/data.ts
@@ -1,15 +1,18 @@
 const dataRaw = [
     {
         category: 'research',
+        summary: 'A dedicated, goal-oriented biomedical engineer with a background in nanomaterial engineering and molecular biology. Expertise in DNA-templated silver nanoclusters probe design, synthesis and application. Extensive experience in gene cloning, transcript expression analysis, recombinant protein expression, purification, and characterization through mutational analysis and functional assays. Excellent problem solving, communication, and multi-tasking skills.',
         jobs: [
             {
                 duration: [new Date('Feb 2014'), new Date(Date.now())],
-                title: 'Visiting Scholar, University of Texas at Austin, <a href="#">Dr. Tim Yeh’s lab</a>',
+                title: 'Visiting Scholar, University of Texas at Austin, Dr. Tim Yeh’s lab',
                 projects: [
                     {
                         projectName: 'Detection of N6-methyladenine with methyladenine-specific NanoCluster Beacon (maNCB)',
                         projectNameShort: 'M6A Project',
                         projectDescription: "N6-methyladenine is recently identified as an epigenetic modification in mammalian genomes. Unlike 5-methylcytosine, there is no simple way for m6A detection. By using NanoCluster Beacon, a type of silver cluster probe, I established robust and hybridization-based method for detecting m6A content at given position.",
+                        projectPublication: 'Y.-A. Chen, J.M. Obliosca, Y.-L. Liu, C. Liu, M.L. Gwozdz, and H.-C. Yeh, "NanoCluster Beacons enable detection of a single N6-methyladenine," Journal of the American Chemical Society 137(33): 10476-10479, 2015.',
+                        projectPublicationLink: 'http://pubs.acs.org/doi/abs/10.1021/jacs.5b06038',
                         projectImg: 'M6A_project.jpg',
                         details: [
                             'Explored different designs of the probe and developed data visualization method',
@@ -22,6 +25,8 @@ const dataRaw = [
                         projectName: 'Detection of rolling-circle amplification products using Nanocluster Beacon (NCB)',
                         projectNameShort: 'RCP Project',
                         projectDescription: 'Rolling Circle Amplification is a versatile tool for DNA replication. I conducted the RCA on solid surface and aquired product signal using NanoCluster Beacon under epifluorescence microscope',
+                        projectPublication: 'S. Juul*, J.M. Obliosca* (equal contribution), C. Liu, Y.-L. Liu, Y.-A. Chen, D.M. Imphean, B. R. Knudsen, Y.-P. Ho, K.W. Leong, and H.-C. Yeh, "NanoCluster Beacons as reporter probes in rolling circle enhanced enzyme activity detection," Nanoscale 7, 8332-8337, 2015.',
+                        projectPublicationLink: 'http://pubs.rsc.org/en/content/articlelanding/2015/nr/c5nr01705j',
                         projectImg: 'RCP_project.jpg',
                         details: [
                             'Conjugated primers onto solid phase, carried out the rolling-circle amplification, and acquired the fluorescent signal with the probe (NCBs and Rohdamine) under epifluorescence microscope.',
@@ -31,6 +36,8 @@ const dataRaw = [
                         projectName: 'Exploring the capability of NanoCluster Beacons for multicolor labeling in human cell line',
                         projectNameShort: 'NCB Optimization Project',
                         projectDescription: 'NanoCluster Beacons are a special design of DNA-templated silver nanocluster the fluoresce upon hybridization with target DNA. I utilized NCB as fluorescent tag to label fixed cell.',
+                        projectPublication: 'J.M. Obliosca, M.C. Babin, C. Liu, Y.-L. Liu, Y.-A. Chen, R.A. Batson, M. Ganguly, J.T. Petty, and H.-C. Yeh, "A complementary palette of NanoCluster Beacons," ACS Nano 8(10): 10150-10160, 2014.',
+                        projectPublicationLink: 'http://pubs.acs.org/doi/abs/10.1021/nn505338e',
                         projectImg: 'NCB_optimization_project.jpg',
                         details: [
                             'Analyzed excitation and emission properties of multicolor NanoCluster Beacons under epifluorescence microscope',
