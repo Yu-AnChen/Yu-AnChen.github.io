f46f401b901fdc2a99e5261f3573648dc2ec3dec
Yu-An Chen <atwood12@gmail.com>
2017-04-17 14:33:43 -0700

    use projects component

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-projects',
@@ -6,7 +6,14 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent {
-  showDetail = {};
+  // showDetail = {};
   @Input() project: any;
+  @Input() showDetail: any;
+  @Output() onShowDetailChange = new EventEmitter<boolean>();
+
   constructor() { }
+
+  showDetailChange(show: boolean) {
+    this.onShowDetailChange.emit(show);
+  }
 }
